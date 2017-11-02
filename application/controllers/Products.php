75befc5a32e644f4ea7e4a9234d4e62f14ada5f7
EllieLeon <elizabethleon1044@gmail.com>
2017-11-01 19:54:53 -0600

    cambios en la pantalla de ordenes

---
+++ application/controllers/Products.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DELEON
+ * Date: 31-Oct-17
+ * Time: 2:57 PM
+ */
+
+class Orders extends CI_Controller
+{
+
+    public  function index()
+    {
+        //devuelve las categorias
+        $data['categoria_data'] = $this->Categoria_model->get_categorias_info();
+
+        echo $this->Producto_model->get_productos_categoria(1);
+
+
+        $data['main_view'] = "orders/register_view";
+
+        $this->load->view('layouts/main', $data);
+    }
+
+}
