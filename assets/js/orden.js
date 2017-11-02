0ec221b302c95b0d66b365218582f07076b65272
José Ramírez <slipknotoy@hotmail.com>
2017-11-02 16:18:05 -0600

    Actulizacion de .live a la funcion .on pues la primera solo es para versiones jquery 1.7 u 1.8

--- assets/js/orden.js
+++ assets/js/orden.js
@@ -7,91 +7,105 @@ var baseurl = 'http://localhost/restaurant/';
 //Variables para el mesero
 var idMeseroActual = 0;
 var nombreMeseroActual = "";
+$(document).ready(function() {
 
-(function(){
+        //aca no comprendo que esta tratando de hacer
+        //seria mejor en lugar de "body" poner la clase del elemento que esta utlizando
+        //por ejemplo  $(".claseelemento").on("click", ".btnCategorias", function (e) {
+        //y lo optimo para este caso seria $(".bntCategoris").click( function(){
+        //todo lo demas queda igual
+
+        $("body").on("click", ".btnCategorias", function (e) {
+            e.preventDefault();//para que no recargue la pagina, no redirecciona con el link
 
 
-    $("body").on("click", ".btnCategorias", function( e ){
-        e.preventDefault();//para que no recargue la pagina, no redirecciona con el link
+            //nombre tomado del data-nombre
+            var id = $(this).data("id");
+            var categoria = $(this).data('categoria');
+            console.log(id);
+            console.log(categoria);
 
+            $.post(baseurl + 'Products/obtener_productos_categoria/' + id, function (data) {
 
-        //nombre tomado del data-nombre
-        var id           = $(this).data("id");
-        var categoria    = $(this).data('categoria');
-        console.log(id);
-        console.log(categoria);
+                var result = JSON.parse(data);
 
-        $.post(baseurl + 'Products/obtener_productos_categoria/' + id, function (data) {
+                console.log(result);
 
-            var result = JSON.parse(data);
+                $.each(result, function (i, val) {
 
-            console.log(result);
+                    var content = "";
+                    content += '<button type="button" class="btn btn-yellow btn-lg btn-block">' + val.producto + '</button>';
 
-            $.each(result, function (i, val) {
+                    $("#contenedor_productos").append(content);
 
-                var content = "";
-                content += '<button type="button" class="btn btn-yellow btn-lg btn-block">' + val.producto +'</button>';
-
-                $("#contenedor_productos").append(content);
-
+                });
             });
+
         });
 
-    });
+
+        //realiazar busqueda de paciente para reporte
+        //hacemos focus al campo de búsqueda
+        $("#mesero").focus();
+        //comprobamos si se pulsa una tecla
+        $("#mesero").keyup(function (e) {
+            var contenido = "";
+            contenido += '<ul class="list-group" id="meseros"></ul>';
+            $("#cargabusqueda").append(contenido);
+
+            var consulta;
+            //obtenemos el texto introducido en el campo de búsqueda
+            consulta = $("#mesero").val();
+            //hace la búsqueda
+            if (consulta != "") {
+                $.post(baseurl + 'Empleados/buscarMesero', {nombre: consulta}, function (mensaje) {
+                    if (mensaje != '') {
+                        $("#meseros").show();
+                        $("#meseros").html(mensaje);
+                        //console.log(mensaje);
+                    } else {
+                        $("#meseros").html('');
+                    }
+                    ;
+                });
+            }
+        });
+
+        //cuando se da click sobre el nombre de algun paciente determinado
+        $(".cargarMesero").live('click', function (e) {
+            e.preventDefault();
+            //capturamos el id del paciente
+            var idMesero = $(this).data("id");
+            //capturamos el nombre del paciente
+            var nombreMesero = $(this).data("nombre");
+
+            //asignamos a la variable global el valor de la variable local
+            idMeseroActual = idMesero;
+            //asignamos a la variable global el valor de la variable local
+            nombreMeseroActual = nombreMesero;
+            //al input tipo text le colocamos el valor de la variable
+            $("#mesero").val(nombreMesero);
+            $("#meseros").remove();
+
+        });
+
+        //siempre que se vaya a dar click sobre un elemtento creado dinamicamente en el DOM
+        //ulitilizareos esta funcion .on en lugar de .live
+        //con atencion a como seteamos el elemento (cargarmesero en este caso)
+        $( document ).on( "click", ".cargarmesero", function (e) {
+        e.preventDefault()
+        var idestudiante = $(this).data("id")
+        var nombreestudiante = $(this).data("nombre")
+
+        console.log(idestudiante)
+        console.log(nombreestudiante)
 
 
-    //realiazar busqueda de paciente para reporte
-    //hacemos focus al campo de búsqueda
-    $("#mesero").focus();
-    //comprobamos si se pulsa una tecla
-    $("#mesero").keyup(function(e)
-    {
-        var contenido = "";
-        contenido +='<ul class="list-group" id="meseros"></ul>';
-        $("#cargabusqueda").append(contenido);
 
-        var consulta;
-        //obtenemos el texto introducido en el campo de búsqueda
-        consulta = $("#mesero").val();
-        //hace la búsqueda
-        if (consulta != "")
-        {
-            $.post(baseurl+'Empleados/buscarMesero', {nombre: consulta}, function(mensaje)
-            {
-                if (mensaje!= '')
-                {
-                    $("#meseros").show();
-                    $("#meseros").html(mensaje);
-                    //console.log(mensaje);
-                } else
-                {
-                    $("#meseros").html('');
-                };
-            });
-        }
-    });
-
-    //cuando se da click sobre el nombre de algun paciente determinado
-    $(".cargarMesero").live('click', function(e)
-    {
-        e.preventDefault();
-        //capturamos el id del paciente
-        var idMesero = $(this).data("id");
-        //capturamos el nombre del paciente
-        var nombreMesero = $(this).data("nombre");
-
-        //asignamos a la variable global el valor de la variable local
-        idMeseroActual = idMesero;
-        //asignamos a la variable global el valor de la variable local
-        nombreMeseroActual = nombreMesero;
-        //al input tipo text le colocamos el valor de la variable
-        $("#mesero").val(nombreMesero);
-        $("#meseros").remove();
-
-    });
+    })
 
 
-})();
+})
 
 
 
